7ca92c12851c802e872390153dcae790d647e130
mcarthur <mcarthur.alford@proton.me>
2023-08-25 21:34:36 +1000

    Functional Grammar and Test Program

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use pest::Parser;
+use pest_derive::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct CSVParser;
+
+fn main() {
+    let unparsed_file = fs::read_to_string("script").expect("cannot read file");
+
+    let file = CSVParser::parse(Rule::file, &unparsed_file).expect("Unsuccessful parse");
+    let tokens = file.tokens();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
