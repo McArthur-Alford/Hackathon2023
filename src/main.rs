916ea978d681338135a58e890edd770c02b834ff
mcarthur <mcarthur.alford@proton.me>
2023-08-25 22:52:19 +1000

    Further language cleanup to ONLY use generics

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,14 @@ struct Prod {
 fn main() {
     let unparsed_file = fs::read_to_string("script").expect("cannot read file");
 
-    let file = CSVParser::parse(Rule::file, &unparsed_file).expect("Unsuccessful parse");
+    let parse_result = CSVParser::parse(Rule::file, &unparsed_file)
+        .expect("Unsuccessful parse")
+        .next()
+        .unwrap();
 
-    dbg!("{:?}", &file.len());
-    dbg!("{:?}", &file);
+    dbg!("{:?}", &parse_result);
 
-    let tokens = file.tokens();
+    let tokens = parse_result.tokens();
 
     for token in tokens {
         println!("{:?}", token);
