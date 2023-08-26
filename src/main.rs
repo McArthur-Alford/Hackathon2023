a4bda6f258188db7ac1f58cb41026dd6878146b8
mcarthur <mcarthur.alford@proton.me>
2023-08-26 10:22:49 +1000

    Functional Alpha Network

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,166 @@ use pest::{
     Parser,
 };
 use pest_derive::Parser;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 pub struct CSVParser;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 struct Variable {
     identifier: String,
 }
 
-#[derive(Debug)]
-enum Value {
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum Literal {
     Text(String),
     Number(i64),
-    Variable(Variable),
     Bool(bool),
 }
 
-#[derive(Debug)]
-struct WME {
-    ident: Option<Variable>,
-    attr: Option<String>,
-    value: Option<Value>,
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum AbstractAttribute {
+    Literal(String),
+    Variable(Variable),
 }
 
-#[derive(Debug)]
-struct Prod {
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum AbstractValue {
+    Literal(Literal),
+    Variable(Variable),
+}
+
+#[derive(Debug, Clone)]
+enum ConcreteValue {
+    Literal(Literal),
+    Variable(EntityId),
+}
+
+#[derive(Debug, Clone)]
+struct AbstractWME {
+    ident: Variable,
+    attr: AbstractAttribute,
+    value: AbstractValue,
+}
+
+#[derive(Debug, Clone)]
+struct ConcreteWME {
+    ident: EntityId,
+    attr: String,
+    value: ConcreteValue,
+}
+
+#[derive(Debug, Clone)]
+struct AbstractProd {
     precedence: usize,
-    lhs: Vec<WME>,
-    rhs: Vec<WME>,
+    lhs: Vec<AbstractWME>,
+    rhs: Vec<AbstractWME>,
 }
 
-fn process_variable(pair: Pair<Rule>) -> Option<Variable> {
+#[derive(Debug, Clone)]
+struct EntityId(usize);
+
+#[derive(Debug)]
+struct AlphaMemory {
+    memories: Vec<ConcreteWME>,
+}
+
+impl AlphaMemory {
+    fn activate(&mut self, cw: ConcreteWME) {
+        // Add the cw into this alpha memory
+        self.memories.push(cw);
+    }
+}
+
+struct WMEStore {}
+
+#[derive(Debug)]
+struct ConstantTestNode {
+    // Test to perform on incoming ConcreteWMEs
+    test: Option<AbstractWME>,
+    // Downstream alpha memories
+    output_memory: Option<AlphaMemory>,
+    // Downstream constant tests
+    children: Vec<Box<ConstantTestNode>>,
+}
+
+// Ensures:
+// cw and aw have the same attribute (if it is literal)
+// cw and aw have the same value IF it is a literal
+fn pattern_match(cw: ConcreteWME, aw: AbstractWME) -> bool {
+    // Match the attr if it is literal
+    if let AbstractAttribute::Literal(lit) = aw.attr {
+        if lit != cw.attr {
+            return false;
+        }
+    }
+    // Match the value if it is literal
+    if let AbstractValue::Literal(lit) = aw.value {
+        if let ConcreteValue::Literal(lit2) = cw.value {
+            if lit != lit2 {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+impl ConstantTestNode {
+    fn activate(&mut self, cw: ConcreteWME) {
+        if let Some(test) = self.test.clone() {
+            if pattern_match(cw.clone(), test) {
+                // CW matched this constant test node pattern
+                // Add it to any directly downstream alpha memory:
+                if let Some(output_memory) = &mut self.output_memory {
+                    output_memory.activate(cw.clone());
+                }
+
+                // Trigger all downstream concrete tests:
+                for child in &mut self.children {
+                    child.activate(cw.clone());
+                }
+            }
+        } else {
+            // CW matched this constant test node pattern
+            // Add it to any directly downstream alpha memory:
+            if let Some(output_memory) = &mut self.output_memory {
+                output_memory.activate(cw.clone());
+            }
+
+            // Trigger all downstream concrete tests:
+            for child in &mut self.children {
+                child.activate(cw.clone());
+            }
+        }
+    }
+}
+
+fn process_variable(pair: Pair<Rule>) -> Variable {
     let mut inner: Pairs<Rule> = pair.into_inner(); // we know this is a variable
     let contents = inner.next().unwrap();
     match contents.clone().as_rule() {
-        Rule::generic => Some(Variable {
+        Rule::generic => {
+            (Variable {
+                identifier: contents
+                    .into_inner()
+                    .next()
+                    .unwrap()
+                    .as_span()
+                    .as_str()
+                    .to_string(),
+            })
+        }
+        _ => panic!(),
+    }
+}
+
+fn process_attr(pair: Pair<Rule>) -> AbstractAttribute {
+    let mut inner: Pairs<Rule> = pair.into_inner();
+    let contents = inner.next().unwrap();
+    match contents.clone().as_rule() {
+        Rule::generic => AbstractAttribute::Variable(Variable {
             identifier: contents
                 .into_inner()
                 .next()
@@ -49,21 +171,12 @@ fn process_variable(pair: Pair<Rule>) -> Option<Variable> {
                 .as_str()
                 .to_string(),
         }),
-        _ => None,
+        _ => AbstractAttribute::Literal(contents.as_span().as_str().to_string()),
     }
+    // AbstractAttribute::Literal("WOO".to_string())
 }
 
-fn process_attr(pair: Pair<Rule>) -> Option<String> {
-    let mut inner: Pairs<Rule> = pair.into_inner();
-    let contents = inner.next().unwrap();
-    if contents.as_rule() == Rule::wildcard {
-        None
-    } else {
-        Some(contents.as_str().to_string())
-    }
-}
-
-fn process_value(pair: Pair<Rule>) -> Option<Value> {
+fn process_value(pair: Pair<Rule>) -> AbstractValue {
     let mut inner = pair.into_inner();
     let contents = inner.next().unwrap();
     match contents.clone().as_rule() {
@@ -77,36 +190,42 @@ fn process_value(pair: Pair<Rule>) -> Option<Value> {
                     chars.next();
                     chars.next_back();
                     let string = chars.as_str().to_string();
-                    Some(Value::Text(string))
+                    (AbstractValue::Literal(Literal::Text(string)))
                 }
-                Rule::number => Some(Value::Number(contents.as_span().as_str().parse().unwrap())),
-                Rule::bool => Some(Value::Bool(match contents.as_span().as_str() {
-                    "True" => true,
-                    _ => false,
-                })),
+                Rule::number => {
+                    (AbstractValue::Literal(Literal::Number(
+                        contents.as_span().as_str().parse().unwrap(),
+                    )))
+                }
+                Rule::bool => {
+                    (AbstractValue::Literal(Literal::Bool(match contents.as_span().as_str() {
+                        "True" => true,
+                        _ => false,
+                    })))
+                }
                 _ => {
                     panic!()
                 }
             }
         }
-        Rule::var => Some(Value::Variable(process_variable(contents)?)),
+        Rule::var => (AbstractValue::Variable(process_variable(contents))),
         _ => {
             panic!()
         }
     }
 }
 
-fn process_wme(pair: Pair<Rule>) -> WME {
+fn process_wme(pair: Pair<Rule>) -> AbstractWME {
     // "Safe" to assume pair IS a WME
     let mut inner: Pairs<Rule> = pair.into_inner();
     let ident = process_variable(inner.next().unwrap());
     let attr = process_attr(inner.next().unwrap());
     let value = process_value(inner.next().unwrap());
 
-    WME { ident, attr, value }
+    AbstractWME { ident, attr, value }
 }
 
-fn process_block(pair: Pair<Rule>) -> Vec<WME> {
+fn process_block(pair: Pair<Rule>) -> Vec<AbstractWME> {
     // "Safe" to assume pair IS a block
     let mut output = Vec::new();
     let mut inner: Pairs<Rule> = pair.into_inner();
@@ -116,20 +235,20 @@ fn process_block(pair: Pair<Rule>) -> Vec<WME> {
     output
 }
 
-fn process_production(pair: Pair<Rule>) -> Prod {
+fn process_production(pair: Pair<Rule>) -> AbstractProd {
     // "Safe" to assume pair IS a production
     let mut inner: Pairs<Rule> = pair.into_inner();
     let id = inner.next().unwrap();
     let lhs = inner.next().unwrap();
     let rhs = inner.next().unwrap();
-    Prod {
+    AbstractProd {
         precedence: 0,
         lhs: process_block(lhs),
         rhs: process_block(rhs),
     }
 }
 
-fn process_file(pair: Pair<Rule>) -> Vec<Prod> {
+fn process_file(pair: Pair<Rule>) -> Vec<AbstractProd> {
     // "Safe" to assume pair IS a file
     let mut output = Vec::new();
     for prod in pair.into_inner() {
@@ -151,4 +270,91 @@ fn main() {
 
     let out = process_file(file);
     dbg!(out);
+
+    let root = ConstantTestNode {
+        test: None,
+        output_memory: None,
+        children: Vec::new(),
+    };
+    dbg!(root);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    #[test]
+    fn test1() {
+        let child = ConstantTestNode {
+            test: Some(AbstractWME {
+                ident: Variable {
+                    identifier: "_".to_string(),
+                },
+                attr: AbstractAttribute::Variable(Variable {
+                    identifier: "_".to_string(),
+                }),
+                value: AbstractValue::Literal(Literal::Number(1)),
+            }),
+            output_memory: Some(AlphaMemory {
+                memories: Vec::new(),
+            }),
+            children: Vec::new(),
+        };
+        let child = ConstantTestNode {
+            test: Some(AbstractWME {
+                ident: Variable {
+                    identifier: "_".to_string(),
+                },
+                attr: AbstractAttribute::Literal("yete".to_string()),
+                value: AbstractValue::Variable(Variable {
+                    identifier: "_".to_string(),
+                }),
+            }),
+            output_memory: Some(AlphaMemory {
+                memories: Vec::new(),
+            }),
+            children: vec![Box::new(child)],
+        };
+        let mut root = ConstantTestNode {
+            test: Some(AbstractWME {
+                ident: Variable {
+                    identifier: "_".to_string(),
+                },
+                attr: AbstractAttribute::Variable(Variable {
+                    identifier: "_".to_string(),
+                }),
+                value: AbstractValue::Variable(Variable {
+                    identifier: "_".to_string(),
+                }),
+            }),
+            output_memory: Some(AlphaMemory {
+                memories: Vec::new(),
+            }),
+            children: vec![Box::new(child)],
+        };
+        dbg!(&root);
+
+        let test_wme = ConcreteWME {
+            ident: EntityId(0),
+            attr: "yote".to_string(),
+            value: ConcreteValue::Literal(Literal::Number(1)),
+        };
+        let test_wme2 = ConcreteWME {
+            ident: EntityId(1),
+            attr: "yete".to_string(),
+            value: ConcreteValue::Literal(Literal::Number(1)),
+        };
+        let test_wme3 = ConcreteWME {
+            ident: EntityId(2),
+            attr: "yete".to_string(),
+            value: ConcreteValue::Literal(Literal::Number(2)),
+        };
+
+        dbg!(&test_wme);
+
+        root.activate(test_wme);
+        root.activate(test_wme2);
+        root.activate(test_wme3);
+
+        dbg!(&root);
+    }
 }
